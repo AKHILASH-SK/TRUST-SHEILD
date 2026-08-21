4d01cab5e66306d820ff7ab54a95e9c3993152ce
AKHILASH-SK <akhilash.sk@gmail.com>
2026-08-21 21:19:38 +0530

    removed hardcoded local ip address

--- app/src/main/java/com/example/trustshield/network/RetrofitClient.kt
+++ app/src/main/java/com/example/trustshield/network/RetrofitClient.kt
@@ -59,7 +59,7 @@ class RetrofitClient private constructor(private val baseUrl: String) {
         /**
          * Get singleton instance of RetrofitClient
          */
-        fun getInstance(baseUrl: String = "http://${BuildConfig.BACKEND_IP}:8000"): RetrofitClient {
+        fun getInstance(baseUrl: String = "https://trust-sheild.onrender.com"): RetrofitClient {
             if (instance == null) {
                 instance = RetrofitClient(baseUrl)
             }
